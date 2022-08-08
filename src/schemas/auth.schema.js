d95d5ebde040c2996574aa28a80a90da9976ed7a
leliovictor <leliov.vieira@gmail.com>
2022-08-08 14:32:31 -0300

    Fixed bug where confirmPassword joi wasn't required

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -4,7 +4,7 @@ const signUp = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-    confirmPassword: joi.ref('password')
+    confirmPassword: joi.string().required().valid(joi.ref('password'))
 });
 
 const signIn = joi.object({
